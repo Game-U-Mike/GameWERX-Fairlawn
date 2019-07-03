498652ce1054971bc71a195a38a37bec6295f2ec
Game-U-Mike <39772905+Game-U-Mike@users.noreply.github.com>
2019-07-03 15:03:49 -0400

    Pause Menu

---
+++ Cat Robot Level Fixed/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public GameObject Pause;
+	bool Paused=false;
+	// Use this for initialization
+	void Start () {
+		if (Cursor.lockState != CursorLockMode.Locked)
+			Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Pause")) {
+			if (Paused) {
+				Cursor.lockState = CursorLockMode.Locked;
+				Time.timeScale = 1;
+				Paused = false;
+				Pause.SetActive (false);
+			} else {
+				Time.timeScale = 0;
+				Paused = true;
+				Pause.SetActive (true);
+				Cursor.lockState = CursorLockMode.None;
+			}
+		}
+	}
+}
